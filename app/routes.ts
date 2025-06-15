7b7d8d98dab1961be34a566e81016091e2a30459
BrunoBianchi <bruno.raiadobian@gmail.com>
2025-06-14 23:15:55 -0300

    feat: first merge for testing routing

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,6 @@
 import { type RouteConfig, index, layout,route } from "@react-router/dev/routes";
 export default [
-    index("layouts/subdomain-layout.tsx"),
+    index("./layouts/subdomain-layout.tsx"),
     layout("./layouts/blogs-layout.tsx",[
         route("post/:id", "./screens/post-screen.tsx"),
     ])
